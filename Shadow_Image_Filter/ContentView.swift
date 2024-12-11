f705756b797919693e57af12e9c2ac307636d5c1
Maxnxi <nisq@ya.ru>
2024-12-11 11:35:05 +0700

    Initial Commit

---
+++ Shadow_Image_Filter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shadow_Image_Filter
+//
+//  Created by Maksim Ponomarev on 12/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shadow_Image_Filter/Shadow_Image_FilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shadow_Image_FilterApp.swift
+//  Shadow_Image_Filter
+//
+//  Created by Maksim Ponomarev on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Shadow_Image_FilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
